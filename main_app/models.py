09a5518a3a41d63bcc8aa5dc56b31eaaf0e8e72f
sandyfromnyc <sandyfromnyc@gmail.com>
2023-03-01 23:32:36 -0500

    added feeding

--- main_app/models.py
+++ main_app/models.py
@@ -21,16 +21,22 @@ class Dog(models.Model):
   def get_absolute_url(self):
     return reverse('detail', kwargs={'dog_id': self.id})
   
-  class Feeding(models.Model):
-    date = models.DateField()
-    meal = models.CharField(
-      max_length=1,
-      choices=MEALS,
-      default=MEALS[0][0]
-      )
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+  max_length=1,
+  choices=MEALS,
+  default=MEALS[0][0]
+  )
     
-    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
 
-    def __str__(self):
-      return f"{self.get_meal_display()} on {self.date}"
+def __str__(self):
+  return f"{self.get_meal_display()} on {self.date}"
+
+class Meta:
+  ordering = ['-date']
+
+def fed_for_today(self):
+  return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
   
