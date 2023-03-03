c97d23cbe4f52e9cacd4ff54659196779d5d6b8c
sandyfromnyc <sandyfromnyc@gmail.com>
2023-03-02 22:13:08 -0500

    added toys

--- main_app/models.py
+++ main_app/models.py
@@ -8,12 +8,24 @@ MEALS = (
     ('D', 'Dinner'),
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+
 # Create your models here.
 class Dog(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return f'{self.name} ({self.id})'  
@@ -21,10 +33,15 @@ class Dog(models.Model):
   def get_absolute_url(self):
     return reverse('detail', kwargs={'dog_id': self.id})
   
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 class Feeding(models.Model):
   date = models.DateField('feeding date')
   meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
 
   def __str__(self):
    return f"{self.get_meal_display()} on {self.date}"
@@ -32,7 +49,4 @@ class Feeding(models.Model):
   class Meta:
     ordering = ['-date']
 
-  def fed_for_today(self):
-    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
   
-dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog
+from django.views.generic import ListView, DetailView
+from .models import Dog, Toy
 from .forms import FeedingForm
 
 
@@ -19,12 +20,14 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
+  id_list = dog.toys.all().values_list('id')
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in=id_list)
   feeding_form = FeedingForm()
-  return render(request, 'dogs/detail.html', { 'dog': dog, 'feeding_form': feeding_form})
+  return render(request, 'dogs/detail.html', { 'dog': dog, 'feeding_form': feeding_form, 'toys': toys_dog_doesnt_have })
 
 class DogCreate(CreateView):
   model  = Dog
-  fields = '__all__'
+  fields = ['name', 'breed', 'description', 'age']
 
 class DogUpdate(UpdateView):
   model  = Dog
@@ -35,6 +38,7 @@ class DogDelete(DeleteView):
   success_url = '/dogs'
 
 def add_feeding(request, dog_id):
+  print(request.POST)
   # create a ModelForm instance using the data submitted in the form
   form = FeedingForm(request.POST)
   if form.is_valid():
@@ -43,3 +47,28 @@ def add_feeding(request, dog_id):
     new_feeding.save()
   return redirect('detail', dog_id=dog_id)
 
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys'
+
+def assoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+def unassoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.remove(toy_id)
+  return redirect('detail', dog_id=dog_id)
